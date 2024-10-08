060149e8165c777295e9a9a15860414d45d3ec95
Heritage <iheritage934@gmail.com>
2024-10-08 21:08:39 +0100

    fixed the button issue

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,6 @@
 const menuBtn = document.getElementById("toggleMenu")
 const menu = document.getElementById("menu")
 menuBtn.addEventListener("click", event => {
-  console.log(event.isTrusted)
   menu.style.display = menu.style.display === "none" ? "flex" : "none" ;
+  
 })
