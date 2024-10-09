96b79aecb5452780f10ef7fcdf3ac76a758c4b83
Heritage <iheritage934@gmail.com>
2024-10-09 21:55:39 +0100

    adding style for pc view

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,12 @@
 
 
-const menuBtn = document.getElementById("toggleMenu")
-const menu = document.getElementById("menu")
+const menuBtn = document.getElementById("toggleMenu");
+const menu = document.getElementById("menu");
+const link = document.getElementById("link");
+const icon = document.getElementById("icon");
+
+
+
 menuBtn.addEventListener("click", event => {
   menu.style.display = menu.style.display === "none" ? "flex" : "none" ;
-  
 })
