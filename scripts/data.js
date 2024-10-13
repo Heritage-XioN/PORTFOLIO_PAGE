8067b4548fb24744fa0fadda8a5fa8c49044860d
Heritage-XioN <iheritage934@gmail.com>
2024-10-13 13:16:27 +0100

    added dynamic rendering of data to the services ans achievement sections. i will update the remaining sections with my next commit

---
+++ scripts/data.js
@@ -0,0 +1,57 @@
+
+export const services = [
+  {
+    id : "01/",
+    title : "Web Development",
+    mainTxt : "With knowledge of HTML5,  CSS, & bootsrap i can make your dream website an instantbreality"
+  },
+  {
+    id : "02/",
+    title : "gelo",
+    mainTxt : "yo just testing" 
+  },
+  {
+    id : "03/",
+    title : "gelo",
+    mainTxt : "yo just testing" 
+  },
+  ]
+   
+export const awards = [
+  {
+    id : "01/",
+    title : "gelo",
+    mainTxt : "yo just testing" 
+  },
+  {
+    id : "02/",
+    title : "gelo",
+    mainTxt : "yo just testing" 
+  },
+  {
+    id : "03/",
+    title : "gelo",
+    mainTxt : "yo just testing" 
+  },
+  ]
+   
+export const awardsCard = [
+  {
+    id : "01/",
+    title : "gelo",
+    mainTxt : "yo just testing" 
+  },
+  {
+    id : "02/",
+    title : "gelo",
+    mainTxt : "yo just testing" 
+  },
+  {
+    id : "03/",
+    title : "gelo",
+    mainTxt : "yo just testing" 
+  },
+  ]
+   
+   
+    
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,54 @@
-
+import { awards , services } from "./data.js"
 
 const menuBtn = document.getElementById("toggleMenu");
 const menu = document.getElementById("menu");
-const link = document.getElementById("link");
-const icon = document.getElementById("icon");
+const awardCard = document.getElementById("awardCard");
+
 
 
 
 menuBtn.addEventListener("click", event => {
   menu.style.display = menu.style.display === "none" ? "flex" : "none" ;
+})
+
+
+
+services.forEach( item => 
+{
+  console.log(item)
+  let html = `
+  <div class="card_cont" id="card">
+        <div class="card_body">
+          <p class="card_num">${item.id}</p>
+          <h5 class="title">
+            <i class="fa-solid fa-code fa-2xl title_icon"></i>
+            ${item.title}
+          </h5>
+          <p class="card-text">
+            ${item.mainTxt}
+          </p>
+          </div>
+        </div>
+  `;
+  servicesCard.innerHTML += html;
+})
+
+awards.forEach( item => 
+{
+  console.log(item)
+  let html = `
+  <div class="card_cont" id="card">
+        <div class="card_body">
+          <p class="card_num">${item.id}</p>
+          <h5 class="title">
+            <i class="fa-solid fa-code fa-2xl title_icon"></i>
+            ${item.title}
+          </h5>
+          <p class="card-text">
+            ${item.mainTxt}
+          </p>
+        </div>
+      </div>
+  `;
+  awardCard.innerHTML += html;
 })
